4b90726b778eb362c4d98f8734e123d7e6aaf34b
Phan Anh Dung <phananhdung309@yahoo.com>
2013-01-05 01:09:02 +0100

    First version of Function
    Need revising on concrete syntax

---
+++ Z3Fs/Function.fs
@@ -0,0 +1,13 @@
+﻿module Microsoft.Z3.FSharp.Function
+
+open System
+open Microsoft.Z3
+open Microsoft.Z3.FSharp.Common
+open Microsoft.Z3.FSharp.Bool
+
+type Microsoft.Z3.FSharp.Bool.Z3 with
+    static member inline CreateFunction< ^T when ^T: (static member FromExpr : Expr -> ^T) and ^T: (static member Sort : Sort)>(name: string, [<ParamArray>] args: Theory []) =
+        let exprs = args |> Array.map (fun arg -> arg.Expr)
+        let sorts = exprs |> Array.map (fun expr -> expr.Sort)
+        let range = (^T : (static member Sort : Sort) ())
+        (^T : (static member FromExpr : Expr -> ^T) (getContext().MkFuncDecl(name, sorts, range).Apply(exprs)))
